fcbd472dbebebb246e74a9943f286004eb491f1f
Dante Baptiste <dantebaptiste@REVI024.local>
2021-08-16 10:36:20 -0500

    added the testing of the hello file

---
+++ test_hello.py
@@ -0,0 +1,18 @@
+import hello as hi
+import unittest
+
+class Test(unittest.TestCase):
+    #1
+    def test_hello_Jaquline(self):
+        print("test_hello_Jaquline")
+        name1 = "Jaquline"
+        self.assertEqual(hi.hello(name1), "Hello, Jaquline")
+
+    #2
+    def test_hello_Blake(self):
+        print("test_hello_Blake")
+        name2 = "Blake"
+        self.assertEqual(hi.hello(name2), "Hello, Blake")
+
+if __name__ == '__main__':
+    unittest.main()
